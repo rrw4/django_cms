3b4ca4d8f89a1bbce1bb7576cdb9b307cce414a7
Roger Wang <r.rj.wang@gmail.com>
2012-05-10 08:39:47 -0500

    SearchKeyword and move search to django_cms directory

--- django_cms/settings.py
+++ django_cms/settings.py
@@ -124,6 +124,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
     'django.contrib.flatpages',
+    'django_cms.search',
 )
 
 # A sample logging configuration. The only tangible logging
