5df1833cf70de331e50107dbbf9e31458c507a36
Roger Wang <r.rj.wang@gmail.com>
2012-06-10 15:42:51 -0400

    separate out blog urls

--- django_cms/urls.py
+++ django_cms/urls.py
@@ -5,6 +5,9 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^search/$', 'django_cms.search.views.search'),
-    url(r'^weblog/', include('django_blog.urls')),
+    url(r'^weblog/categories/', include('django_blog.urls.categories')),
+    url(r'^weblog/links/', include('django_blog.urls.links')),
+    url(r'^weblog/tags/', include('django_blog.urls.tags')),
+    url(r'^weblog/', include('django_blog.urls.entries')),
     url(r'', include('django.contrib.flatpages.urls')),
 )
